c316cb856c33aa87ca87bbae47a6571b6b720d31
Manuel Riel <m@nuelriel.com>
2014-04-19 17:44:27 +0800

    Add keychain integration and update HPN patch.

--- hpn-ssh.rb
+++ hpn-ssh.rb
@@ -2,23 +2,39 @@ require 'formula'
 
 class HpnSsh < Formula
   homepage 'http://www.openssh.com/'
-  url 'http://ftp5.eu.openbsd.org/ftp/pub/OpenBSD/OpenSSH/portable/openssh-6.3p1.tar.gz'
-  version '6.3p1'
-  sha1 '70845ca79474258cab29dbefae13d93e41a83ccb'
+  url 'http://anga.funkfeuer.at/ftp/pub/OpenBSD/OpenSSH/portable/openssh-6.5p1.tar.gz'
+  version '6.5p1'
+  sha1 '3363a72b4fee91b29cf2024ff633c17f6cd2f86d'
 
   option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+  option 'with-keychain-support', 'Add native OS X Keychain and Launch Daemon support to ssh-agent'
 
   depends_on 'openssl' if build.with? 'brewed-openssl'
+  depends_on 'autoconf' => :build if build.with? 'keychain-support'
   depends_on 'ldns' => :optional
   depends_on 'pkg-config' => :build if build.with? "ldns"
 
   conflicts_with 'openssh'
 
   def patches
-    'http://downloads.sourceforge.net/project/hpnssh/HPN-SSH%2014v2%206.3p1/openssh-6.3p1-hpnssh14v2.diff.gz'
+    p = []
+    # Apply a revised version of Simon Wilkinson's gsskex patch (http://www.sxw.org.uk/computing/patches/openssh.html), which has also been included in Apple's openssh for a while
+    p << 'https://gist.github.com/kruton/8951373/raw/a05b4a2d50bbac68e97d4747c1a34b53b9a941c4/openssh-6.5p1-apple-keychain.patch' if build.with? 'keychain-support'
+    p << 'http://downloads.sourceforge.net/project/hpnssh/HPN-SSH%2014v4%206.5p1/openssh-6.5p1-hpnssh14v4.diff.gz'
+    p
   end
 
+  # def patches
+  #   'http://downloads.sourceforge.net/project/hpnssh/HPN-SSH%2014v4%206.5p1/openssh-6.5p1-hpnssh14v4.diff.gz'
+  # end
+
   def install
+    system "autoreconf -i" if build.with? 'keychain-support'
+
+    if build.with? "keychain-support"
+      ENV.append "CPPFLAGS", "-D__APPLE_LAUNCHD__ -D__APPLE_KEYCHAIN__"
+      ENV.append "LDFLAGS", "-framework CoreFoundation -framework SecurityFoundation -framework Security"
+    end
 
     args = %W[
       --with-libedit
@@ -32,5 +48,21 @@ class HpnSsh < Formula
     system "./configure", *args
     system "make"
     system "make install"
+  end
+
+  def caveats
+    if build.with? "keychain-support" then <<-EOS.undent
+        For complete functionality, please modify:
+          /System/Library/LaunchAgents/org.openbsd.ssh-agent.plist
+
+        and change ProgramArguments from
+          /usr/bin/ssh-agent
+        to
+          #{HOMEBREW_PREFIX}/bin/ssh-agent
+
+        After that, you can start storing private key passwords in
+        your OS X Keychain.
+      EOS
+    end
   end
 end
