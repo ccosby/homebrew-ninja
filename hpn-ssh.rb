69d520ba37aedb5d2408a0e9678d6f2dedc0c2ea
Chris Cosby <chris.cosby@aptplatforms.com>
2016-08-29 11:21:01 -0400

    [hpn-ssh] Remove deprecated usage of sha1 and Formula.factory

--- hpn-ssh.rb
+++ hpn-ssh.rb
@@ -4,7 +4,7 @@ class HpnSsh < Formula
   homepage 'http://www.openssh.com/'
   url 'http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.5p1.tar.gz'
   version '6.5p1'
-  sha1 '3363a72b4fee91b29cf2024ff633c17f6cd2f86d'
+  sha256 'a1195ed55db945252d5a1730d4a2a2a5c1c9a6aa01ef2e5af750a962623d9027'
 
   option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
   option 'with-keychain-support', 'Add native OS X Keychain and Launch Daemon support to ssh-agent'
@@ -21,13 +21,13 @@ class HpnSsh < Formula
     # which has also been included in Apple's openssh for a while.
     # https://gist.github.com/kruton/8951373
     url 'https://gist.github.com/kruton/8951373/raw/a05b4a2d50bbac68e97d4747c1a34b53b9a941c4/openssh-6.5p1-apple-keychain.patch'
-    sha1 '28b175507688db38a8c543f4afdb31b5ca994eeb'
+    sha256 'b2d5a3ef6436cea6060aa227872ba4fdad97242a36800322eaa70d9bbf3895d1'
   end if build.with? 'keychain-support'
 
   patch do
     # The HPN-SSH patch installs over the Apple Keychain patch
     url 'http://downloads.sourceforge.net/project/hpnssh/HPN-SSH%2014v4%206.5p1/openssh-6.5p1-hpnssh14v4.diff.gz'
-    sha1 '63080015f0222bd7a0726da59b397230e0a81bc4'
+    sha256 '7b0507172759dd2c85965728981343d4b60d8b8c5faf2c20dc8145606421c364'
   end
 
   def install
@@ -43,7 +43,7 @@ class HpnSsh < Formula
       --prefix=#{prefix}
     ]
 
-    args << "--with-ssl-dir=#{Formula.factory('openssl').opt_prefix}" if build.with? 'brewed-openssl'
+    args << "--with-ssl-dir=#{Formulary.factory('openssl').opt_prefix}" if build.with? 'brewed-openssl'
     args << "--with-ldns" if build.with? "ldns"
     args << "--without-openssl-header-check"
 
