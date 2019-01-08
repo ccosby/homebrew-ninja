004a2c926393e9a27e601f6e956e8e940ad18a4e
Chris Cosby <chris.cosby@aptplatforms.com>
2019-01-08 10:36:23 -0500

    [rancher-cli@0.6] New formula for rancher-cli@0.6.12

---
+++ rancher-cli@0.6.rb
@@ -0,0 +1,21 @@
+class RancherCliAT06 < Formula
+  desc "The Rancher CLI is a unified tool to manage your Rancher server"
+  homepage "https://github.com/rancher/cli"
+  url "https://github.com/rancher/cli/archive/v0.6.12.tar.gz"
+  sha256 "423acf937cdda1b5e32f46051a2b3d1d4a29e9124bc84bbc95272b8bd4349a46"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/rancher/cli/").install Dir["*"]
+    system "go", "build", "-ldflags",
+           "-w -X github.com/rancher/cli/version.VERSION=#{version}",
+           "-o", "#{bin}/rancher",
+           "-v", "github.com/rancher/cli/"
+  end
+
+  test do
+    system bin/"rancher", "help"
+  end
+end
